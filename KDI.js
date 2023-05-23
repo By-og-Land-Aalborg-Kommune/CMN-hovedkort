151b32bb07d1bdbf90cec87e6849f553c7fa2257
hrosenskjold <47818937+hrosenskjold@users.noreply.github.com>
2023-05-23 13:21:41 +0200

    Update KDI.js

--- KDI.js
+++ KDI.js
@@ -1,27 +1,29 @@
 $(document).ready(function() {
-    $.getJSON('data/KDI13.geojson', function(data) {
-      L.geoJson(data, {
-        pointToLayer: function(feature, latlng) {
-          var iconUrl = 'img/Kyst.png'; // standard ikon
-          var popupContent = '<h4>Tilladelse fra Kystdirektoratet</h4>' +
-                            '<b>Stednavn:</b> ' + feature.properties.Stednavn +
-                             '<br><b>Type:</b> ' + feature.properties.Type +
-                             '<br><b>Formål:</b> ' + feature.properties.Formål;
+  $.getJSON('data/KDI13.geojson', function(data) {
+    L.geoJson(data, {
+      pointToLayer: function(feature, latlng) {
+        var iconUrl = 'img/Kyst.png'; // standard ikon
+        var popupContent = '<h4>Tilladelse fra Kystdirektoratet</h4>' +
+                          '<b>Stednavn:</b> ' + feature.properties.Stednavn +
+                           '<br><b>Type:</b> ' + feature.properties.Type +
+                           '<br><b>Formål:</b> ' + feature.properties.Formål;
 
-          if (feature.properties.Type === 'Ålegræs tilladelse') {
-            iconUrl = 'img/ålegræs u. tekst.png';
-          } else if (feature.properties.Type === 'Stenrev tilladelse' || feature.properties.Type === 'Stenrev') {
-            iconUrl = 'img/stenrev m. ring hvid.png';
-          }
-  
-          return L.marker(latlng, {
-            icon: L.icon({
-              iconUrl: iconUrl,
-              iconSize: [18, 18],
-              iconAnchor: [5, 5]
-            })
-          }).bindPopup(popupContent);
+        if (feature.properties.Type === 'Ålegræs tilladelse') {
+          iconUrl = 'img/ålegræs u. tekst.png';
+        } else if (feature.properties.Type === 'Stenrev tilladelse' ) {
+          iconUrl = 'img/stenrev m. ring hvid.png';
+        } else {
+          return null; // returnerer null, hvis der ikke skal vises en markør
         }
-      }).addTo(map);
-    });
+
+        return L.marker(latlng, {
+          icon: L.icon({
+            iconUrl: iconUrl,
+            iconSize: [18, 18],
+            iconAnchor: [5, 5]
+          })
+        }).bindPopup(popupContent);
+      }
+    }).addTo(map);
   });
+});
